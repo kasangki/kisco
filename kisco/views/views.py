1da7baea9929294e63605a1900e12f26a4886902
skkan <skkang1970@gmail.com>
2021-03-10 09:20:56 +0900

    2021년 3월 10일

--- kisco/views/views.py
+++ kisco/views/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+import pandas as pd
 
+from kisco.models import TbSmartopSum
+
+from django.http.response import HttpResponse
+import simplejson as json
 
 '''
  * Created by : 강상기
@@ -14,5 +19,12 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     def get(self, request, *args, **kwargs):
 
-        return render(request, 'index.html')
+        smart_op_sum_list = TbSmartopSum.objects.order_by('op_num')[:100]
+        #smart_op_sum_df = pd.DataFrame(list(smart_op_sum))
+        context = {
+            'smart_op_sum_list' : smart_op_sum_list
+        }
+        #print(smart_op_sum_df)
+        #return HttpResponse(json.dumps(context, default=str), content_type="application/json")
+        return render(request, 'index.html', context=context)
 
