83101e343e87cf5b545af938ab97f7314bd3e82d
skkan <skkang1970@gmail.com>
2021-05-09 20:46:13 +0900

    2021년 4월 1일

---
+++ kisco/anaytics/db_util.py
@@ -0,0 +1,41 @@
+class DBUtil():
+    def __init__(self):
+        pass
+
+    ## 변수 타입별 변수 목록 조회
+    def get_var_name_list(self, var_type, all_var_name_df):
+        # 선택변수 목록
+        select_var_name_df = all_var_name_df[all_var_name_df['var_type'] == var_type]
+
+        select_var_name_list = list(select_var_name_df['var_name'])
+        select_var_code_list = list(select_var_name_df['var_code'])
+        select_var_list = []
+        for i in range(len(select_var_code_list)):
+            temp = [select_var_code_list[i], select_var_name_list[i]]
+            select_var_list.append(temp)
+        return select_var_list    ## 변수 타입별 변수 목록 조회
+
+
+    def set_y_final_value_list(self, var_type, all_var_name_df,y_final_result_dict,var_name_list):
+        # 선택변수 목록
+        select_var_name_df = all_var_name_df[all_var_name_df['var_type'] == var_type]
+
+        select_var_name_list = list(select_var_name_df['var_name'])
+        select_var_code_list = list(select_var_name_df['var_code'])
+        select_var_list = []
+        for i in range(len(select_var_code_list)):
+            var_code = select_var_code_list[i]
+            var_name = select_var_name_list[i]
+
+            # 입력변수 존재 여부 체크
+            if(var_code in var_name_list) :
+                is_in = 'Y'
+            else :
+                is_in = 'N'
+            # 코드, 코드명, 값
+            temp = [var_code, var_name,y_final_result_dict[var_code],is_in]
+            select_var_list.append(temp)
+        return select_var_list
+
+
+
--- kisco/views/views.py
+++ kisco/views/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 import pandas as pd
 
+from kisco.anaytics.db_util import DBUtil
 from kisco.models import TbSmartopSum
 from kisco.models import TbVarMap
 from kisco.models import TbModel
+
+from django.db import connection
+
 
 
 from datetime import datetime
@@ -24,7 +28,14 @@ class IndexView(TemplateView):
     def get(self, request, *args, **kwargs):
         # 타입코드 
         # steel_out_vol : 출강량
+        # power_factor : 역률
+        # total_elec_charge : 전력량
         # steel_out_rate : 회수율
+        
+        target_value_names = ['steel_out_vol', 'power_factor', 'total_elec_charge', 'steel_out_rate']
+        target_value_kor_names = ['출강량', '역률', '합계전력량', '회수율']        
+        target_value_code = target_value_names[3]
+        target_value_name = target_value_kor_names[3]
 
         #전체변수목록
         all_var_name = TbVarMap.objects.exclude(var_code = 'op_num').values()
@@ -37,10 +48,6 @@ class IndexView(TemplateView):
         all_var_name_df = pd.DataFrame(list(all_var_name))
         all_var_name_df.sort_values('seq')
 
-
-
-
-
         # 모든변수
         all_var_name_list = list(all_var_name_df['var_name'])
         all_var_code_list = list(all_var_name_df['var_code'])
@@ -49,12 +56,12 @@ class IndexView(TemplateView):
             temp = [all_var_code_list[i],all_var_name_list[i]]
             var_list.append(temp)
 
-
-        ttt_var_list = self.get_var_name_list('T',all_var_name_df)   ## 시간변수목록
-        steel_var_list = self.get_var_name_list('S', all_var_name_df)  ## 고철종류 변수목록
-        mat_var_list = self.get_var_name_list('M', all_var_name_df)   ## 부자재 변수목록
-        equip_var_list = self.get_var_name_list('Q', all_var_name_df)  ## 설비변수 목록
-        etc_var_list = self.get_var_name_list('E', all_var_name_df)   ## 기타 변수 목록
+        db_util = DBUtil()
+        ttt_var_list = db_util.get_var_name_list('T',all_var_name_df)   ## 시간변수목록
+        steel_var_list = db_util.get_var_name_list('S', all_var_name_df)  ## 고철종류 변수목록
+        mat_var_list = db_util.get_var_name_list('M', all_var_name_df)   ## 부자재 변수목록
+        equip_var_list = db_util.get_var_name_list('Q', all_var_name_df)  ## 설비변수 목록
+        etc_var_list = db_util.get_var_name_list('E', all_var_name_df)   ## 기타 변수 목록
 
 
 
@@ -73,6 +80,9 @@ class IndexView(TemplateView):
             temp = [x_list[i],y_list[i]]
             data_list.append(temp)
 
+        ## 모델 목록조회
+        model = TbModel.objects.filter(target_code=target_value_code).values()
+        model_list = list(model)
 
         context = {
             'var_list' : var_list,
@@ -82,12 +92,15 @@ class IndexView(TemplateView):
             'equip_var_list': equip_var_list,
             'etc_var_list': etc_var_list,
             'data_list': data_list,
+            'target_value_code': target_value_code,
+            'target_value_name': target_value_name,
+            'model_list' : model_list,
         }
        
         return render(request, 'index.html', context=context)
 
 
-    ## 변수 타입별 변수 목록 조회    
+    ## 변수 타입별 변수 목록 조회
     def  get_var_name_list(self,var_type,all_var_name_df):
         # 선택변수 목록
         select_var_name_df = all_var_name_df[all_var_name_df['var_type'] == var_type]
@@ -99,6 +112,24 @@ class IndexView(TemplateView):
             temp = [select_var_code_list[i], select_var_name_list[i]]
             select_var_list.append(temp)
         return select_var_list
+
+    ## 변수 타입별 변수 목록 조회
+    def  get_var_name_query_list(self,target_code,target_num, var_type):
+        # 선택변수 목록
+        query = '''select a.target_code ,
+                       a.var_code ,
+                       b.var_name
+                from  tb_var_info a, tb_var_map b
+                where a.target_code = %s
+                and a.target_num = %s
+                and a.var_code = b.var_code
+                and b.var_type = %s '''
+        cursor = connection.cursor()
+        cursor.execute(query, (target_code, target_num,var_type))
+        var_info_list = cursor.fetchall()
+
+
+        return var_info_list
 
 
 class IndexMainDataAna(TemplateView) :
@@ -202,10 +233,9 @@ class IndexMainDataAna(TemplateView) :
             'model_list' : model_list
         }
 
-
-        #print(smart_op_sum_df)
-        #return HttpResponse(json.dumps(context, default=str), content_type="application/json")
         return render(request, 'index.html', context=context)
+
+
 
 class PredictAnalyticView(TemplateView) :
     def get(self, request, *args, **kwargs):
