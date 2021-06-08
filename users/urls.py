60dff28347c1f7db4faf71d43c2da65e2ceb8547
skkan <skkang1970@gmail.com>
2021-06-08 10:57:11 +0900

    Merge remote-tracking branch 'origin/master'

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('logout/', views.logout),
     path('user_list/', views.users_list,name='user_list'),
     path('register/', views.register),
-    path('user_update/', views.user_update),
+    path('user_update/', views.user_update,name='user_update'),
     path('user_delete/P(?P<str:pk>[^/]+)$', views.user_delete,name='user_delete'),
     path('user_detail/P(?P<str:pk>[^/]+)$', views.user_detail,name='user_detail'),
 
--- users/views.py
+++ users/views.py
@@ -86,7 +86,39 @@ def user_detail(request, pk):
     return render(request, 'user_detail.html', {'user': user})
 
 def user_update(request):
-    pass
+
+    user_id = request.POST.get('user_id', None)
+    user_name = request.POST.get('user_name', None)
+    email_addr = request.POST.get('email_addr', None)
+    position_name = request.POST.get('position_name', None)
+    phone_number = request.POST.get('phone_number', None)
+    user_level = request.POST.get('user_level', None)
+    password = request.POST.get('password', None)
+    re_password = request.POST.get('re-password', None)
+    create_id = request.session['user_id']
+
+    res_data = {}
+
+    if not (user_id and user_name and email_addr and position_name and user_level and password and re_password):
+        res_data['error'] = '모든 값을 입력해야합니다.'
+    elif password != re_password:
+        res_data['error'] = '비밀번호가 다릅니다.'
+    else:
+        user = TBUsers(
+            user_id=user_id,
+            password=password,
+            user_name=user_name,
+            email_addr=email_addr,
+            position_name=position_name,
+            phone_number=phone_number,
+            user_level=user_level,
+            create_id=create_id,
+            update_id=create_id
+        )
+
+        user.save()
+
+    return redirect('/users/user_list')
 
 def user_delete(request,pk):
 
